ab7a03b5afb22e64e36160f91463d41015d0ef44
Fabian Rupin <fabian.rupin@wheelwork.fr>
2022-05-01 17:03:21 +0200

    create user route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
 import Login from "../views/LoginView.vue";
+import User from "../views/UserView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/login",
       name: "login",
       component: Login,
+    },
+    {
+      path: "/user",
+      name: "user",
+      component: User,
     },
   ],
 });
---
+++ src/views/UserView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const myToken = JSON.parse(localStorage.getItem("user"));
+
+function parseJwt(token: string) {
+  try {
+    return JSON.parse(atob(token.split(".")[1]));
+  } catch (e) {
+    return null;
+  }
+}
+
+const user = parseJwt(myToken.token)
+
+</script>
+
+}
+<template>
+  <div class="App">
+    <header class="App-header">
+      <h1>Bonjour {{ user.username }}</h1>
+      <hr />
+      <h3>Votre rôle est {{ user.roles }}</h3>
+    </header>
+  </div>
+</template>
+
+<style>
+
+.App-header {
+  padding-left: 200px;
+  padding-right: 200px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App {
+  text-align: center;
+}
+</style>
